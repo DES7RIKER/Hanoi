86a79501caebcb6090a2827f885cdfbdf1b51a3d
Elias <eGasparArellano@gmail.com>
2018-02-25 15:28:17 -0600

    Movements implementation done, Hanoi is working (it takes 6837 instructions with 8 disks)

--- hanoi.asm
+++ hanoi.asm
@@ -4,7 +4,7 @@
 .data 
 .text
 #########################################	Main	#########################################
-	ori $s0, $s0, 3 # 'n' Number of disks
+	ori $s0, $s0, 8 		# 'n' Number of disks (It is a requirement that $s0 contains N)
 
 # POINTERS TO TOWERS
 ###########################
@@ -23,8 +23,10 @@ Initialize:
 	addi $s5, $s5, 4          	# Tower pointer increase
 	add $t1, $t1, -1          	# Disk N-1 
 	bne $t1, $zero, Initialize	#
-	addi  $s5, $s5, -4    		# Returns pointer to last disk in Initial tower
 ###########################
+
+	addi  $t2, $s5, -4    		# Saves pointer to last disk in Initial tower
+	ori $s5, $t0, 0x0000		# Returns pointer to the beginning of Initial tower
 
 Expand:	      
 ###########################
@@ -32,11 +34,12 @@ Expand:
 	beq $t1, $zero, continue	# If the initial value of the second tower is not zero
 	addi $s6, $s6, 32     		# it means that tower 1(initial) invaded addresses of tower 2, 
 	addi $s7, $s7, 64     		# so it is necessary to move base pointers of tower 2 and 3.
-	j Expand              
-	continue:                
+	j Expand                             
 ###########################
-	add $a0, $zero, $s0
-	add $a1, $zero, $s5
+
+continue: 
+	add $a0, $zero, $s0		# Prepare arguments to call Hanoi
+	add $a1, $zero, $t2		# $a1, $a2, $a3 store the address of the last disk stacked in them
 	add $a2, $zero, $s6
 	add $a3, $zero, $s7
 	jal Hanoi
@@ -49,13 +52,22 @@ Hanoi:
 	sw $ra, 0($sp)
 	sw $a0, 4($sp)
 
+	# Base case
 	bne $a0, 1, else
+	
 	# Move ini to end
+	# When you load data you need to ask if address has something inside it, if not, you decrement address + offset condition
 	lw $t0, 0($a1)
-	sw $t0, 0($a3)
 	sw $zero, 0($a1)
-	addi $a1, $a1, -4#??
-	addi $a3, $a3, 4#??
+	beq $a1, $s5, noDec
+	beq $a1, $s6, noDec
+	beq $a1, $s7, noDec
+	addi $a1, $a1, -4
+noDec:	# When you store data you don't want to overwrite information, if address has something inside it, you increment address
+	lw $t1, 0($a3)
+	beq $t1, $zero, write
+	addi $a3, $a3, 4
+write:	sw $t0, 0($a3)
 	j return
 	
 else:	
@@ -69,12 +81,19 @@ else:
 	add $a2, $zero, $a3		# Aux <- End
 	add $a3, $zero, $t0		# End <- Aux 
 	
-	# Move ini to end
+		# Move ini to end
+	# When you load data you need to ask if address has something inside it, if not, you decrement address + offset condition
 	lw $t0, 0($a1)
-	sw $t0, 0($a3)
 	sw $zero, 0($a1)
-	addi $a1, $a1, -4#
-	addi $a3, $a3, 4#
+	beq $a1, $s5, noDec2
+	beq $a1, $s6, noDec2
+	beq $a1, $s7, noDec2
+	addi $a1, $a1, -4
+noDec2:	# When you store data you don't want to overwrite information, if address has something inside it, you increment address
+	lw $t1, 0($a3)
+	beq $t1, $zero, write2
+	addi $a3, $a3, 4
+write2:	sw $t0, 0($a3)
 	
 	# N is already decremented
 	add $t0, $zero, $a2		# Save aux in a temporal register
@@ -86,7 +105,6 @@ else:
 	add $a1, $zero, $t0		# Ini <- Aux 
 
 return: 
-
 	lw $ra, 0($sp)
 	lw $a0, 4($sp)
 	addi $sp, $sp, 8
